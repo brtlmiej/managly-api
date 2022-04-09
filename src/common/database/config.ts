ff75ecd698e49ec17fb49da0f7e1a51beb177ce2
Bartłomiej Ciołek <b.ciolek@gbxsoft.com>
2022-04-09 16:31:34 +0200

    prepare heroku config

--- src/common/database/config.ts
+++ src/common/database/config.ts
@@ -2,12 +2,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export function databaseConfig(): TypeOrmModuleOptions {
   return {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    type: 'sqlite',
+    database: 'db.sqlite3',
     synchronize: true,
     entities: ['dist/**/*.entity{.ts,.js}']
   }
